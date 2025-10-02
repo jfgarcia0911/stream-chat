f2003cd9d7d8c564b0bbb4c87629c4875f6ef991
jfgarcia0911 <jfgarcia0911@gmail.com>
2025-10-03 01:29:12 +0800

    feat(ui): add shadcn sidebar-04 component for app sidebar layout
    - Integrated sidebar-04 from shadcn/ui using npx shadcn@latest add sidebar-04
    - Updated AppSidebar to use new sidebar components for navigation and user actions

--- app/(signed-in)/dashboard/page.tsx
+++ app/(signed-in)/dashboard/page.tsx
@@ -1,12 +1,9 @@
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
 import React from 'react'
 
 export default function Dashboard() {
   return (
     <div>
       dasboard
-      <Link href='./'>Home</Link>
     </div>
   )
 }
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,50 @@
+'use client'
+import * as React from "react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { Button } from "./ui/button"
+import { UserButton, useUser } from "@clerk/nextjs"
+
+
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const {user} = useUser()
+  return (
+    <Sidebar variant="floating" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <div  className="flex items-center justify-between w-full">
+                <div className="flex flex-col">
+                  <span className="text-xs text-muted-foreground">Welcome back</span>
+                  <span className="text-sm font-semibold">{user?.firstName} {user?.lastName}</span>
+                </div>
+                <UserButton signInUrl="/sign-in"/>
+              </div>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu className="gap-2">
+           <Button className="w-full " variant="outline">
+            Start New Chat
+           </Button>
+
+           {/* Channels List */}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
